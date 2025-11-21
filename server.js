ddbd7486af0b7d1b34d8055d539aca8535c9ad2d
shrinidhi2220-hub <shrinidhi2220@gmail.com>
2025-11-21 14:46:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./db.js";
+import authRoutes from "./authRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hostel Backend Running ✔");
+});
+
+// API routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
+
